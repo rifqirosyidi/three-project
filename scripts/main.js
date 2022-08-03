52497e5595a051c4e8fcfd4d35f65a9449d2c7c8
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2022-08-03 12:48:59 +0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { generateText } from "./utils.js";
 
 const skyLeft = new URL('/images/skybox/left.png', import.meta.url).href
@@ -35,7 +35,7 @@ const uniforms = {
 }
 
 // Objects
-let box, box2, box2Obj, sphere;
+let box, box2, box2Obj, box3, box3Obj, octahedron, octaObj, octaMoon, octaObj2, octaMoon2;
 
 init()
 animate()
@@ -43,31 +43,67 @@ animate()
 function init() {
   renderer = new THREE.WebGLRenderer();
   scene = new THREE.Scene()
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 20)
 
   renderer.shadowMap.enabled = true
-  camera.position.set(0, 0, 10)
+  // const control = new OrbitControls(camera, renderer.domElement)
+
+  camera.position.set(0, 0, 21)
+  // control.update
 
   const skyBox = new THREE.CubeTextureLoader()
     .load([
-      skyLeft,
-      skyRight,
-      skyFront,
-      skyBack,
       skyTop,
       skyBottom,
+      skyFront,
+      skyBack,
+      skyRight,
+      skyLeft,
     ]);
 
   scene.background = skyBox
 
-  const textureLoader = new THREE.TextureLoader()
+  const welcomeText = generateText({
+    text: "welcome to",
+    size: 0.25,
+    position: [0, 4, 0]
+  })
+  scene.add(welcomeText)
 
-  const greetingText = generateText(
-    "Hi, Welcome to \nMy Portfolio :)",
-    [0, 30, 30],
-    [0, 0, 0]
-  );
+  const greetingText = generateText({
+    text: "Rief Planetary-System",
+    size: 0.5,
+    position: [0, 3, 0]
+  });
   scene.add(greetingText);
+
+  const scrollText = generateText({
+    text: "scroll to explore",
+    size: 0.25,
+    position: [0, -3, 0]
+  });
+  scene.add(scrollText)
+
+  const hiText = generateText({
+    text: "hi, i am",
+    size: 0.15,
+    position: [0, 4, 18]
+  })
+  scene.add(hiText)
+
+  const nameText = generateText({
+    text: "Rifqi Rosyidi",
+    size: 0.5,
+    position: [0, 3, 18]
+  });
+  scene.add(nameText);
+
+  const bioText = generateText({
+    text: "Web Developer, Designer",
+    size: 0.15,
+    position: [0, -3, 18]
+  });
+  scene.add(bioText);
 
   const boxGeo = new THREE.BoxGeometry()
   const boxMat = new THREE.MeshPhongMaterial({ color: 0x504A4B, shininess: 100, reflectivity: 100 })
@@ -78,30 +114,61 @@ function init() {
   const box2Geo = new THREE.BoxGeometry(0.3, 0.3, 0.3)
   const box2Mat = new THREE.MeshPhongMaterial({ color: 0x605A5b, shininess: 100, reflectivity: 100 })
   box2 = new THREE.Mesh(box2Geo, box2Mat)
+  box3 = box2.clone()
+
   box2Obj.add(box2)
   scene.add(box2Obj)
   box2Obj.rotation.x = THREE.MathUtils.degToRad(10)
   box2.position.x = 4
 
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+  box3Obj = new THREE.Object3D()
+  box3Obj.add(box3)
+  box3Obj.rotation.set(0, 0, 0.3)
+  box3.position.y = 6
+  scene.add(box3Obj)
+
+  const octaGeo = new THREE.OctahedronGeometry()
+  const octaMat = new THREE.MeshPhongMaterial({ color: 0x504A4B, shininess: 100, reflectivity: 100 })
+  octahedron = new THREE.Mesh(octaGeo, octaMat)
+  scene.add(octahedron)
+  octahedron.position.set(0, 0, 15)
+
+  octaObj = new THREE.Object3D()
+  octaObj2 = new THREE.Object3D()
+  octaObj.position.set(0, 0, 15)
+  octaObj2.position.set(0, 0, 15)
+
+  const octaMoonGeo = new THREE.OctahedronGeometry(0.2)
+  octaMoon = new THREE.Mesh(octaMoonGeo, octaMat)
+  octaMoon2 = octaMoon.clone()
+
+  octaObj.add(octaMoon)
+  octaObj2.add(octaMoon2)
+
+  octaMoon.position.x = 5
+  octaMoon2.position.x = 3.5
+
+  scene.add(octaObj)
+  scene.add(octaObj2)
+
+
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.25)
   scene.add(ambientLight)
 
   // ==== DIRECTIONAL LIGHT ===
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
   scene.add(directionalLight)
   directionalLight.position.multiplyScalar(10)
-  directionalLight.position.set(-30, 20, 20)
+  directionalLight.position.set(30, 20, 20)
   directionalLight.castShadow = true
   directionalLight.shadow.camera.bottom = -12
 
   // const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
   // scene.add(dLightShadowHelper)
 
-  scene.fog = new THREE.FogExp2(0x222222, 0.02)
-
-
-
+  scene.fog = new THREE.FogExp2(0x222222, 0.04)
 
 
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -123,12 +190,28 @@ function animate() {
   box2.rotation.y += 0.02
   box2.rotation.x -= 0.001
 
+  box3.rotation.y -= 0.001
+  box3.rotation.x += 0.03
+
+  octaObj.rotation.y += 0.005
+  octaObj.rotation.x += 0.001
+
+  octaObj2.rotation.y += 0.01
+  octaObj2.rotation.x += 0.001
+
+  octahedron.rotation.y += 0.005
+  octahedron.rotation.x += 0.001
+
+  octaMoon.rotation.y += 0.01
+
   box2Obj.rotation.y += 0.01
+  box3Obj.rotation.x += 0.003
+  box3Obj.rotation.y += 0.003
 
 
 
-  target.x = (1 - mouse.x) * 0.0003;
-  target.y = (1 - mouse.y) * 0.0003;
+  target.x = (1 - mouse.x) * 0.0001;
+  target.y = (1 - mouse.y) * 0.0001;
 
   camera.rotation.x += 0.005 * (target.y - camera.rotation.x);
   camera.rotation.y += 0.005 * (target.x - camera.rotation.y);
@@ -146,6 +229,9 @@ function onMouseMove(event) {
 
 function onMouseWheel(event) {
   camera.position.z += event.deltaY * 0.01; // move camera along z-axis
+  if (camera.position.z < 10) {
+    camera.position.z = 10
+  }
 }
 
 function onResize(event) {
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,9 +1,9 @@
 import * as THREE from "three"
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader"
 
-const fontUrl = new URL("../fonts/josefin.json", import.meta.url).href;
+const fontUrl = new URL("/fonts/nasa.json", import.meta.url).href;
 
-export function generateText(text, position, rotation = [0, 0, 0], hasSide = false) {
+export function generateText({ text, size = 1, position = [0, 0, 0], rotation = [0, 0, 0], hasSide = false }) {
   let displayedText = new THREE.Object3D();
   const fontLoader = new FontLoader();
   fontLoader.load(fontUrl, (font) => {
@@ -13,7 +13,7 @@ export function generateText(text, position, rotation = [0, 0, 0], hasSide = fal
       side: hasSide && THREE.DoubleSide,
     });
 
-    const shapes = font.generateShapes(text, 5);
+    const shapes = font.generateShapes(text, size);
     const geo = new THREE.ShapeBufferGeometry(shapes);
     geo.computeBoundingBox();
     const xMid = -0.5 * (geo.boundingBox.max.x - geo.boundingBox.min.x);
